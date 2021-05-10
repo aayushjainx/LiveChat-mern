1611cf6f8028418f2c765518466913085ceac018
Aayush Jain <jaayush014@gmail.com>
2021-05-11 01:31:50 +0530

    project setup

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAsKLWxHCBcGbDm7oa39947c1YeOk0C1cU',
+  authDomain: 'live-chat-mern.firebaseapp.com',
+  projectId: 'live-chat-mern',
+  storageBucket: 'live-chat-mern.appspot.com',
+  messagingSenderId: '298123615442',
+  appId: '1:298123615442:web:e912f9af6464f49711210b',
+  measurementId: 'G-CSZ0RSTWC3',
+};
