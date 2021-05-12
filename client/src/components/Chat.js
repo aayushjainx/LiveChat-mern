3fa89aba88d45054bfe04a61f2f3e31fe9c8d033
Aayush Jain <jaayush014@gmail.com>
2021-05-13 02:12:08 +0530

    Chat box done

---
+++ client/src/components/Chat.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import '../styles/Chat.css';
+import { Avatar, IconButton } from '@material-ui/core';
+import { AttachFile, InsertEmoticon, Mic, MoreVert, SearchOutlined } from '@material-ui/icons';
+
+function Chat() {
+  return (
+    <div className='chat'>
+      <div className='chat__header'>
+        <Avatar />
+
+        <div className='chat__headerInfo'>
+          <h3>Room name</h3>
+          <p>Last Seen..</p>
+        </div>
+
+        <div className='chat__headerRight'>
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className='chat__body'>
+        <p className='chat__message'>
+          <span className='chat__name'> Aayush</span> This is a message
+          <span className='chat__timestamp'>{new Date().toUTCString()}</span>
+        </p>
+
+        <p className='chat__message chat__reciever'>
+          <span className='chat__name'> Aayush</span> This is a message
+          <span className='chat__timestamp'>{new Date().toUTCString()}</span>
+        </p>
+
+        <p className='chat__message'>
+          <span className='chat__name'> Aayush</span> This is a message
+          <span className='chat__timestamp'>{new Date().toUTCString()}</span>
+        </p>
+      </div>
+
+      <div className='chat__footer'>
+        <InsertEmoticon />
+        <form>
+          <input placeholder='Type a message' type='text' />
+          <button type='submit'>Send a message</button>
+        </form>
+        <Mic />
+      </div>
+    </div>
+  );
+}
+
+export default Chat;
--- src/components/Chat.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import '../styles/Chat.css';
-
-function Chat() {
-  return <div className='chat'>chat</div>;
-}
-
-export default Chat;
